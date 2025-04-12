0b9862c3cbfcf586e40f7fce4ed12a85a4204ab7
oluwaseun <mendoseroluwaseun@gmail.com>
2025-04-12 09:26:45 -0400

    Updated DevicesScreen

---
+++ app/src/main/java/com/example/fittrack/ui/screens/workouts/DevicesScreen.kt
@@ -0,0 +1,9 @@
+package com.example.fittrack.ui.screens.devices
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DevicesScreen() {
+    Text("Devices Screen")
+}
